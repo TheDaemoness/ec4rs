b55d52b541f7d41f06d73b29385ee8eed55f0b79
TheDaemoness <TheDaemoness@users.noreply.github.com>
2022-02-22 00:39:07 -0800

    Add a Properties map

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,3 @@
+mod properties;
 
+pub use properties::Properties;
---
+++ src/properties.rs
@@ -0,0 +1,52 @@
+use patricia_tree::PatriciaMap;
+
+/// A map of property names to property values.
+/// When iterated over, returns key/value pairs in the order the keys were first seen.
+/// This structure is case-sensitive.
+/// It's the caller's responsibility to ensure all keys and values are lowercased.
+#[derive(Clone)]
+pub struct Properties {
+	keys: Vec<String>,
+	map: PatriciaMap<String>
+}
+
+impl Properties {
+	/// Construct a new, empty Properties object.
+	pub fn new() -> Properties {
+		Properties {
+			keys: Vec::new(),
+			map: PatriciaMap::new()
+		}
+	}
+
+	/// Retrieve the value for the specified key.
+	pub fn get(&self, key: impl AsRef<str>) -> Option<&str> {
+		self.map.get(key.as_ref()).map(|x| x.as_str())
+	}
+
+	/// Returns an iterator over the key-value pairs, ordered from oldest key to newest key.
+	pub fn iter(&self) -> impl Iterator<Item = (&str, &str)> {
+		self.keys.iter().map(|key| {
+			(key.as_ref(), self.get(key).unwrap())
+		})
+	}
+
+	/// Set the value for a specified key.
+	pub fn set(&mut self, key: impl AsRef<str>, value: String) -> &mut Self {
+		let key_str = key.as_ref();
+		if self.map.insert(key_str, value).is_none() {
+			self.keys.push(key_str.to_owned())
+		}
+		self
+	}
+}
+
+impl<K: AsRef<str>, V: Into<String>> FromIterator<(K, V)> for Properties {
+	fn from_iter<T: IntoIterator<Item = (K, V)>>(iter: T) -> Self {
+		let mut result = Properties::new();
+		for (k, v) in iter {
+			result.set(k, v.into());
+		}
+		result
+	}
+}
