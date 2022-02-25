a8b310bd3d0bb91784089d313ca3c17259f2c16e
TheDaemoness <TheDaemoness@users.noreply.github.com>
2022-02-25 08:58:30 -0800

    Improve line reader
    Added accessors for the line number and line.
    The parse_line method is now a free-standing function (for testing).
    The line reader now rejects incomplete key-value pairs.
    The line reader now rejects empty sections.

--- src/linereader.rs
+++ src/linereader.rs
@@ -1,15 +1,47 @@
 use std::io as io;
 
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum Line<'a> {
 	Nothing,
 	Section(&'a str),
 	Pair(&'a str, &'a str),
 }
 
+#[derive(Debug)]
 pub enum LineFail {
 	EOF,
 	IoError(io::Error),
-	Invalid(usize)
+	Invalid
+}
+
+type LineReadResult<'a> = Result<Line<'a>, LineFail>;
+
+pub fn parse_line(line: &str) -> LineReadResult<'_> {
+	let mut l = line.trim_start();
+	if l.starts_with(|c| c == ';' || c == '#') {
+		Ok(Line::Nothing)
+	} else {
+		l = l.trim_end();
+		if l.is_empty() {
+			Ok(Line::Nothing)
+		} else if let Some(s) = l.strip_prefix('[').and_then(|s| s.strip_suffix(']')) {
+			if s.is_empty() {
+				Err(LineFail::Invalid)
+			} else {
+				Ok(Line::Section(s))
+			}
+		} else if let Some((key_raw, val_raw)) = l.split_once('=') {
+			let key = key_raw.trim_end();
+			let val = val_raw.trim_start();
+			if key.is_empty() || val.is_empty() {
+				Err(LineFail::Invalid)
+			} else {
+				Ok(Line::Pair(key.trim_end(), val.trim_start()))
+			}
+		} else {
+			Err(LineFail::Invalid)
+		}
+	}
 }
 
 pub struct LineReader<R: io::Read> {
@@ -27,22 +59,20 @@ impl<R: io::Read> LineReader<R> {
 		}
 	}
 
-	pub fn parse_line(&self) -> Result<Line<'_>, LineFail> {
-		let l = self.line.trim();
-		if l.is_empty() {
-			Ok(Line::Nothing)
-		} else if l.starts_with(|c| c == ';' || c == '#') {
-			Ok(Line::Nothing)
-		} else if let Some(s) = l.strip_prefix('[').and_then(|s| s.strip_suffix(']')) {
-			Ok(Line::Section(s))
-		} else if let Some((key, val)) = l.split_once('=') {
-			Ok(Line::Pair(key.trim_end(), val.trim_start()))
-		} else {
-			Err(LineFail::Invalid(self.ticker))
-		}
+	pub fn line_no(&self) -> usize {
+		self.ticker
 	}
 
-	pub fn next(&mut self) -> Result<Line<'_>, LineFail> {
+	pub fn line(&self) -> &str {
+		self.line.as_str()
+	}
+
+	// Convenience method for `parse_line(self.line())
+	pub fn reparse(&self) -> LineReadResult<'_> {
+		parse_line(self.line())
+	}
+
+	pub fn next(&mut self) -> LineReadResult<'_> {
 		self.line.clear();
 		use std::io::BufRead;
 		match self.reader.read_line(&mut self.line) {
@@ -50,7 +80,7 @@ impl<R: io::Read> LineReader<R> {
 			Ok(0) => Err(LineFail::EOF),
 			Ok(_) => {
 				self.ticker += 1;
-				self.parse_line()
+				self.reparse()
 			}
 		}
 	}
