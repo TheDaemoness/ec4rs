619607f7c0a84d6e896b45b2d7951901d77aa33a
TheDaemoness <TheDaemoness@users.noreply.github.com>
2022-03-01 16:59:43 -0800

    Implement ec4rs-parse

--- tools/src/bin/ec4rs-parse.rs
+++ tools/src/bin/ec4rs-parse.rs
@@ -6,8 +6,8 @@ use clap::Parser;
 #[clap(disable_version_flag = true)]
 struct Args {
 	/// Override config filename
-	#[clap(default_value = ".editorconfig", short)]
-	filename: OsString,
+	#[clap(short)]
+	filename: Option<OsString>,
 	/// Ignored by this implementation
 	#[clap(default_value = ec4rs::version::STRING, short = 'b')]
 	ec_version: String,
@@ -17,11 +17,25 @@ struct Args {
 	files: Vec<std::path::PathBuf>
 }
 
+fn print_config(path: &std::path::Path, filename: Option<&OsString>) {
+	if let Ok(props) = ec4rs::get_config_for(path, filename) {
+		for (key, value) in props.iter() {
+			println!("{} = {}", key, value);
+		}
+	}
+	// TODO: Error reporting!!!
+}
+
 fn main() {
-	let mut args = Args::parse();
+	let args = Args::parse();
 	if args.version {
 		println!("EditorConfig (ec4rs-parse {}) Version {}", env!("CARGO_PKG_VERSION"), ec4rs::version::STRING);
+	} else if args.files.len() == 1 {
+		print_config(args.files.first().unwrap(), args.filename.as_ref());
 	} else {
-		eprintln!("NYI!");
+		for path in args.files {
+			println!("[{}]", path.to_string_lossy());
+			print_config(&path, args.filename.as_ref());
+		}
 	}
 }
