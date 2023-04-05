1f7521ce183f58e663c56129c28ae42793298d8c
TheDaemoness <she@daemoness.dev>
2023-04-04 20:16:55 -0700

    Bump supported EditorConfig version

--- src/version.rs
+++ src/version.rs
@@ -1,7 +1,12 @@
 //! Information about the version of the EditorConfig specification this library complies with.
+//!
+//! The constants in this module specify the latest version of EditorConfig that ec4rs
+//! is known to be compliant with.
+//! Compliance is determined by running the ec4rs_parse tool
+//! against the same core test suite used by the reference implementation of EditorConfig.
 #![allow(missing_docs)]
 
-pub static STRING: &str = "0.14.0";
+pub static STRING: &str = "0.15.1";
 pub static MAJOR: usize = 0;
-pub static MINOR: usize = 14;
-pub static PATCH: usize = 0;
+pub static MINOR: usize = 15;
+pub static PATCH: usize = 1;
